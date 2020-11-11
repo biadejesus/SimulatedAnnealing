269a610957d04605c30fb0a01f57f4c4bb173e0e
biadejesus <beatriz.jesus.costa@gmail.com>
2020-11-11 17:12:55 -0300

    rodei os testes

--- paralelo/scriptbrd.sh
+++ paralelo/scriptbrd.sh
@@ -6,17 +6,12 @@ mpicc sap.c -o sap.out -lm -g
 
 echo paralelo compilado
 
-mpirun -n 1 ./sap.out -t 1400000 -a brd14051.tsp > brdpar1_1.txt
-mpirun -n 1 ./sap.out -t 1400000 -a brd14051.tsp > brdpar1_2.txt
-mpirun -n 1 ./sap.out -t 1400000 -a brd14051.tsp > brdpar1_3.txt
-mpirun -n 2 ./sap.out -t 1400000 -a brd14051.tsp > brdpar2_1.txt
-mpirun -n 2 ./sap.out -t 1400000 -a brd14051.tsp > brdpar2_2.txt
-mpirun -n 2 ./sap.out -t 1400000 -a brd14051.tsp > brdpar2_3.txt
-mpirun -n 3 ./sap.out -t 1400000 -a brd14051.tsp > brdpar3_1.txt
-mpirun -n 3 ./sap.out -t 1400000 -a brd14051.tsp > brdpar3_2.txt
-mpirun -n 3 ./sap.out -t 1400000 -a brd14051.tsp > brdpar3_3.txt
-mpirun -n 4 ./sap.out -t 1000000 -a brd14051.tsp > brdpar4_1.txt
-mpirun -n 4 ./sap.out -t 1400000 -a brd14051.tsp > brdpar4_2.txt
-mpirun -n 4 ./sap.out -t 1400000 -a brd14051.tsp > brdpar4_3.txt
+mpirun -n 1 ./sap.out -t 1000 -a brd14051.tsp > brdpar1_1.txt
+
+mpirun -n 2 ./sap.out -t 1000 -a brd14051.tsp > brdpar2_1.txt
+
+mpirun -n 3 ./sap.out -t 1000 -a brd14051.tsp > brdpar3_1.txt
+
+mpirun -n 4 ./sap.out -t 1000 -a brd14051.tsp > brdpar4_1.txt
 
 echo acaboooo
--- paralelo/scriptpla.sh
+++ paralelo/scriptpla.sh
@@ -6,17 +6,9 @@ mpicc sap.c -o sap.out -lm -g
 
 echo paralelo compilado
 
-mpirun -n 1 ./sap.out -t 1400000 -a pla7397.tsp > plapar1_1.txt
-mpirun -n 1 ./sap.out -t 1400000 -a pla7397.tsp > plapar1_2.txt
-mpirun -n 1 ./sap.out -t 1400000 -a pla7397.tsp > plapar1_3.txt
-mpirun -n 2 ./sap.out -t 1400000 -a pla7397.tsp > plapar2_1.txt
-mpirun -n 2 ./sap.out -t 1400000 -a pla7397.tsp > plapar2_2.txt
-mpirun -n 2 ./sap.out -t 1400000 -a pla7397.tsp > plapar2_3.txt
-mpirun -n 3 ./sap.out -t 1400000 -a pla7397.tsp > plapar3_1.txt
-mpirun -n 3 ./sap.out -t 1400000 -a pla7397.tsp > plapar3_2.txt
-mpirun -n 3 ./sap.out -t 1400000 -a pla7397.tsp > plapar3_3.txt
-mpirun -n 4 ./sap.out -t 1000000 -a pla7397.tsp > plapar4_1.txt
-mpirun -n 4 ./sap.out -t 1400000 -a pla7397.tsp > plapar4_2.txt
-mpirun -n 4 ./sap.out -t 1400000 -a pla7397.tsp > plapar4_3.txt
+mpirun -n 1 ./sap.out -t 1000 -a pla7397.tsp > plapar1_1.txt
+mpirun -n 2 ./sap.out -t 1000 -a pla7397.tsp > plapar2_1.txt
+mpirun -n 3 ./sap.out -t 1000 -a pla7397.tsp > plapar3_1.txt
+mpirun -n 4 ./sap.out -t 1000 -a pla7397.tsp > plapar4_1.txt
 
 echo acaboooo
